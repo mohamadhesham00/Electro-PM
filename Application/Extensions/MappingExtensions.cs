f07e5e557958d25c34441682c7a799b4bb882c4a
mohamadhesham00 <mohamadhesham112@gmail.com>
2026-05-20 21:48:36 +0300

    Add MappingExtensions with PatchInto method
    Introduced a new `MappingExtensions` class in the `Application.Extensions` namespace. Added the `PatchInto<TSource, TDestination>` extension method to copy non-null and non-empty string property values from a source object to a destination object using reflection. Ensured only writable properties in the destination object are updated.

---
+++ Application/Extensions/MappingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace Application.Extensions
+{
+    public static class MappingExtensions
+    {
+        public static void PatchInto<TSource, TDestination>(this TSource source, TDestination destination)
+        {
+            var sourceProperties = typeof(TSource).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var destType = typeof(TDestination);
+
+            foreach (var sourceProp in sourceProperties)
+            {
+                var sourceValue = sourceProp.GetValue(source);
+
+                // Skip if the property wasn't provided (null or empty string)
+                if (sourceValue == null || (sourceValue is string s && string.IsNullOrEmpty(s)))
+                    continue;
+
+                // Find the matching property on the destination entity
+                var destProp = destType.GetProperty(sourceProp.Name, BindingFlags.Public | BindingFlags.Instance);
+
+                if (destProp != null && destProp.CanWrite)
+                {
+                    destProp.SetValue(destination, sourceValue);
+                }
+            }
+        }
+    }
+}
