19a6803e976933ef70e1ac287e2300871915a3b6
mohamadhesham00 <mohamadhesham112@gmail.com>
2026-05-20 17:01:51 +0300

    Enable hot reload and improve development workflow
    Updated `entrypoint.sh` to use `dotnet watch run` for hot reload during development. Enhanced `docker-compose.yml` with additional environment variables for application configuration, including JWT settings. Added a `develop` section to enable file synchronization for development directories.

--- API/entrypoint.sh
+++ API/entrypoint.sh
@@ -5,4 +5,4 @@ echo "=> Running database migrations inside the container..."
 dotnet ef database update --project Infrastructure/Infrastructure.csproj --startup-project API/API.csproj --configuration Release
 
 echo "=> Database ready! Starting Electro-PM Web API..."
-exec dotnet run --project API/API.csproj --configuration Release --no-build
+exec dotnet watch run --project API/API.csproj --configuration Debug
